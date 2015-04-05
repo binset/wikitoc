44688ec23da7a92af4fcc4b9810dfd3c63c7372f
teamrc <teamrc@gmx.com>
2015-04-05 23:30:56 +1000

    First version to be checked into mozilla addons

--- wes/lib/main.js
+++ wes/lib/main.js
@@ -167,8 +167,8 @@ function init() {
 
 init()
 
-tabs.open("https://en.wikipedia.org/wiki/Telephone_numbers_in_Australia");
-tabs.open("https://en.wikipedia.org/wiki/Telstra");
+//tabs.open("https://en.wikipedia.org/wiki/Telephone_numbers_in_Australia");
+//tabs.open("https://en.wikipedia.org/wiki/Telstra");
 
 tabs.on('activate', function () {
     if (/wikipedia/.test(tabs.activeTab.url))
